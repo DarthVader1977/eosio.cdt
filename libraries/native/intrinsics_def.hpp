634a63173458d9a8d4d26e3e0f84930454c8f8e1
Darth Vader <jimak@bk.ru>
2020-01-08 22:23:47 +0300

    fixed crash build cdt

--- libraries/native/intrinsics_def.hpp
+++ libraries/native/intrinsics_def.hpp
@@ -156,7 +156,7 @@ intrinsic_macro(send_context_free_inline) \
 intrinsic_macro(send_deferred) \
 intrinsic_macro(cancel_deferred) \
 intrinsic_macro(get_context_free_data) \
-intrinsic_macro(random_number)
+intrinsic_macro(generate_random)
 
 #define CREATE_ENUM(name) \
    name,
