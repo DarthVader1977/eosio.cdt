651f880fb7e4c83c4e993133086cdc823d9ff9c0
Darth Vader <jimak@bk.ru>
2020-10-25 14:59:08 +0300

    v2

--- libraries/native/native/eosio/intrinsics_def.hpp
+++ libraries/native/native/eosio/intrinsics_def.hpp
@@ -10,6 +10,7 @@
 #include <eosio/system.h>
 #include <eosio/transaction.h>
 #include <eosio/types.h>
+#include <eosio/random.h>
 
 #include <type_traits>
 
@@ -158,7 +159,8 @@ intrinsic_macro(send_deferred) \
 intrinsic_macro(cancel_deferred) \
 intrinsic_macro(get_context_free_data) \
 intrinsic_macro(get_sender) \
-intrinsic_macro(set_action_return_value)
+intrinsic_macro(set_action_return_value)\
+intrinsic_macro(random_number)
 
 #define CREATE_ENUM(name) \
    name,
